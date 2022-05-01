017279f09845c67a164d1393764f2dc622806a22
daniyal019 <daniyal019@users.noreply.github.com>
2022-05-01 17:54:42 +0500

    3rd commit

--- app/src/main/java/com/example/loginlayout/MainActivity.java
+++ app/src/main/java/com/example/loginlayout/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.loginlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button2 = (Button) findViewById(R.id.button2);
+        TextView txt= (TextView) findViewById(R.id.txt);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                txt.setText("Login Successful");
+            }
+        });
     }
 }
